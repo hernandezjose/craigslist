ea62919c617f2969e0e7aed05f350879b12dbbdf
Jose Hernandez <hernandez.was.here@gmail.com>
2017-07-20 21:28:08 -0500

    updated pause placing in cragislist

--- craigslist.py
+++ craigslist.py
@@ -9,9 +9,9 @@ db = TinyDB(Constants.DATABASE())
 engine = SearchEngine(db)
 
 while True:
-    time.sleep(Constants.PAUSE())
     with open(Constants.LOGFILE(), 'a') as log:
         date = str(datetime.datetime.now())
         log.write(date + '\n')
         print ('\n====================', date, '====================\n')
     listings = engine.search()
+    time.sleep(Constants.PAUSE())
